92aa28b578f3a78de84cf2917544adb750492aac
Stéphane Bruckert <contact@stephanebruckert.com>
2014-10-09 22:17:29 +0200

    Can scrobble to Last.fm

---
+++ SBShazamScrobbler/LastFmConstants.m
@@ -0,0 +1,21 @@
+//
+//  LastFmConstants.m
+//  SBShazamScrobbler
+//
+//  Created by Stéphane Bruckert on 09/10/14.
+//  Copyright (c) 2014 Stéphane Bruckert. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "LastFmConstants.h"
+
+@implementation LastFmConstants : NSObject
+
+NSString* const APPNAME = @"ShazamScrobbler";
+NSString* const APIKEY = @"0be923a204f2aed08a1bdd15876e9836";
+NSString* const SECRET = @"";
+
+NSString* const USERNAME = @"";
+NSString* const PASSWORD = @"";
+
+@end
